2b9ed631826fde426397f6fe2ec5b859a86edd3f
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-10-03 17:29:41 -0300

    Componente Header criado e renderizano na página Wallet.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email, totalExpense } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <h3>TRYBEWALLET</h3>
+
+        <span data-testid="email-field">{email}</span>
+        <div>
+          <span>Despesa Total: </span>
+          <span data-testid="total-field">{totalExpense}</span>
+          <span data-testid="header-currency-field"> BRL</span>
+        </div>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string,
+  // totalExpense: PropTypes.number,
+}.isRequired;
+
+Header.defaultProps = {
+  totalExpense: 0,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
