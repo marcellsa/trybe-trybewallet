0dfbfc5b92821eb7fcf20ff0cb53f2a613aac3b9
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-10-13 11:41:37 -0300

    Testes para atingir 60% de cobertura foramd esenvolvidos.

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 // import { Provider } from 'react-redux';
 // import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import Login from '../pages/Login';
+import App from '../App';
 
 describe('Teste a página de Login', () => {
   test('Se os campos de email e senha estão presentes na página', () => {
@@ -21,5 +23,50 @@ describe('Teste a página de Login', () => {
 
     const button = screen.getByRole('button', { name: /entrar/i });
     expect(button).toBeInTheDocument();
+  });
+
+  test('Se o botão está desabilitado', () => {
+    renderWithRouterAndRedux(<Login />);
+
+    const button = screen.getByRole('button', { name: /entrar/i });
+    expect(button).toBeDisabled();
+  });
+
+  test('Se o botão foi habilitado', () => {
+    renderWithRouterAndRedux(<Login />);
+
+    const emailTest = 'test@login.com';
+    const passwordTest = '123456';
+
+    const email = screen.getByText(/email/i);
+    const password = screen.getByText(/senha/i);
+
+    userEvent.type(email, emailTest);
+    userEvent.type(password, passwordTest);
+
+    const button = screen.queryByText('Entrar');
+    expect(button).toBeEnabled();
+  });
+
+  test('Se ao clicar no botão, é redirecionado para página Wallet', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const emailTest = 'test@login.com';
+    const passwordTest = '123456';
+
+    const email = screen.getByText(/email/i);
+    const password = screen.getByText(/senha/i);
+
+    userEvent.type(email, emailTest);
+    userEvent.type(password, passwordTest);
+
+    const button = screen.getByRole('button', { name: /entrar/i });
+    userEvent.click(button);
+
+    expect(history.location.pathname).toBe('/carteira');
+    // setTimeout(() => {
+    //   expect(pathname).toBe('/carteira');
+    // }, 3000);
   });
 });
--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 // import { Provider } from 'react-redux';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
 
@@ -26,10 +26,41 @@ describe('Teste a página de Wallet', () => {
     expect(button).toBeInTheDocument();
   });
 
+  test('Se o input Valor de despesa aparece na tabela da página Wallet', () => {
+    renderWithRouterAndRedux(<Wallet />);
+
+    const valueInput = screen.getByTestId('value-input');
+    expect(valueInput).toBeInTheDocument();
+  });
+
+  test('Se o campo Descrição aparece na tabela da página Wallet', () => {
+    renderWithRouterAndRedux(<Wallet />);
+
+    const fieldDescription = screen.getByTestId('description-input');
+    expect(fieldDescription).toBeInTheDocument();
+  });
+
   test('Se o campo Tag aparece na tabela da página Wallet', () => {
     renderWithRouterAndRedux(<Wallet />);
 
     const fieldTag = screen.getByRole('columnheader', { name: /tag/i });
     expect(fieldTag).toBeInTheDocument();
+  });
+
+  test('Se ao adicionar informações, botão de excluir aparece na tabela', () => {
+    renderWithRouterAndRedux(<Wallet />);
+
+    const value = screen.getByRole('textbox', { name: /descrição da despesa/i });
+    userEvent.type(value, 'Cinema');
+
+    const button = screen.getByRole('button', { name: /adicionar despesa/i });
+    userEvent.click(button);
+
+    // const valueCell = screen.getByRole('cell', { name: /cinema/i });
+    // const valueCell = screen.getByRole('cell', { name: /excluir editar/i });
+    setTimeout(() => {
+      const valueCell = screen.getByTestId('delete-btn');
+      expect(valueCell).toBeInTheDocument();
+    }, 3000);
   });
 });
